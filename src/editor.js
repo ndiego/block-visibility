8bc6591ae5c1c832bccc902604317d4a4c611a76
Nick Diego <ndiego@outermostdesign.com>
2020-05-19 22:16:29 -0400

    started work on the editor controls

--- src/editor.js
+++ src/editor.js
@@ -1,9 +1,16 @@
 /**
+ * External dependencies
+ */
+import { assign } from 'lodash';
+
+/**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
 import { addFilter } from "@wordpress/hooks";
+import { Component, render } from '@wordpress/element';
 import { 
+	ToggleControl,
 	SelectControl,
  	PanelBody
 } from "@wordpress/components";
@@ -11,6 +18,109 @@ import { InspectorControls } from "@wordpress/editor";
 import { createHigherOrderComponent } from "@wordpress/compose";
 import { useEntityProp } from '@wordpress/core-data';
 
+
+const blockVisibilityAttribute = ( settings ) => {
+	
+	//console.log( settings.attributes );
+	
+	settings.attributes = assign( settings.attributes, {
+		blockVisibility: {
+			type: 'object',
+			properties: {
+				hideBlock: {
+					type: 'boolean',
+				},
+				hideLoggedIn: {
+					type: 'boolean',
+				},
+				hideLoggedOut: {
+					type: 'boolean',
+				}	
+			},
+			default: {
+				hideBlock: true,
+				hideLoggedIn: false,
+				hideLoggedOut: false,
+			}
+		}
+	} );
+	
+	return settings;
+}
+
+// Add the visibility attributes to all blocks.
+addFilter(
+	'blocks.registerBlockType',
+	'outermost/block-visibility/block-visibility-attribute',
+	blockVisibilityAttribute
+);
+
+
+class EditorVisibilityControls extends Component {
+	
+	constructor() {
+		super( ...arguments );
+	}
+	
+	render() {
+		
+		const {
+			attributes,
+			setAttributes
+		} = this.props;
+		
+		const {
+			blockVisibility,
+		} = attributes;
+		
+		console.log( this.props );
+		
+		return(
+			<PanelBody
+				title={ __( 'Visibility', 'block-visibility' ) }
+				initialOpen={ false }
+			>
+				<ToggleControl
+					label={ __(
+						'Hide block',
+						'block-visibility'
+					) }
+					checked={ blockVisibility.hideBlock }
+					onChange={ () => {
+						const atts = { ...blockVisibility };
+						atts.hideBlock = ! blockVisibility.hideBlock;
+						setAttributes( { blockVisibility: atts } );
+					} }
+				/>
+				<ToggleControl
+					label={ __(
+						'Hide for Logged In Users',
+						'block-visibility'
+					) }
+					checked={ blockVisibility.hideBlock }
+					onChange={ () => {
+						const atts = { ...blockVisibility };
+						atts.hideBlock = ! blockVisibility.hideBlock;
+						setAttributes( { blockVisibility: atts } );
+					} }
+				/>
+				<ToggleControl
+					label={ __(
+						'Hide for Logged Out Users',
+						'block-visibility'
+					) }
+					checked={ blockVisibility.hideBlock }
+					onChange={ () => {
+						const atts = { ...blockVisibility };
+						atts.hideBlock = ! blockVisibility.hideBlock;
+						setAttributes( { blockVisibility: atts } );
+					} }
+				/>
+				All of the visibility settings
+			</PanelBody>
+		)
+	}
+}
 
 const blockVisibilityControls = createHigherOrderComponent( ( BlockEdit ) => {
     return ( props ) => {
@@ -23,17 +133,12 @@ const blockVisibilityControls = createHigherOrderComponent( ( BlockEdit ) => {
 		);
 		
 		// Wait till disabledBlocks are loaded, then make sue the block is not part of the array
-		if ( disabledBlocks && ! disabledBlocks.includes( props.name ) ) {
+		if ( disabledBlocks && ! disabledBlocks.includes( props.name ) ) {			
 			return (
 				<>
 					<BlockEdit { ...props } />
 					<InspectorControls>
-						<PanelBody
-							title={ __( 'Visibility', 'block-visibility' ) }
-							initialOpen={ false }
-						>
-							All of the visibility settings
-						</PanelBody>
+						<EditorVisibilityControls { ...props } />
 					</InspectorControls>
 				</>
 			);
@@ -52,3 +157,8 @@ addFilter(
 	blockVisibilityControls,
 	100
 );
+
+
+
+
+
