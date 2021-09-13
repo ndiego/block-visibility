3b517eb4da6031bb2dc1b86ac844175c3f66866f
Nick Diego <ndiego@outermost.co>
2021-09-13 06:35:52 -0400

    Fix rule display condition bug.

--- src/editor/inspector-controls/utils/rule-sets/rule.js
+++ src/editor/inspector-controls/utils/rule-sets/rule.js
@@ -219,7 +219,7 @@ export default function Rule( props ) {
 										condition.dependencyType ===
 										'operatorField'
 											? 'operator'
-											: 'value';
+											: 'subField';
 
 									fieldValue = rule[ fieldType ] ?? '';
 								}
