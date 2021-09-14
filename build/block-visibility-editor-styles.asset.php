54593bda273fdfa13959e6b3fff685e53d87c0a1
Nick Diego <ndiego@outermost.co>
2021-09-14 08:10:42 -0400

    Fresh build of Block Visibility.

--- build/block-visibility-editor-styles.asset.php
+++ build/block-visibility-editor-styles.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '2109aeacdd7c1363e3eb9ffdcdf1ebba');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'ca53c82b8e9c3328604639ba73aaac46');
