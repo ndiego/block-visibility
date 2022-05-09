9b23a6bc9f75e92b001401b8f5df8a279e0940c3
Nick Diego <ndiego@outermost.co>
2022-05-09 08:46:47 -0400

    Fresh build.

--- build/block-visibility-editor.asset.php
+++ build/block-visibility-editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => '945c001532a854ea3251ac63132f5757');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => '6d7de98cfb0e7e470f6f442e3820e715');
