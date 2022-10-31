2cf5e4093daae31cc1f3436dad6370315a4c2928
Nick Diego <ndiego@outermost.co>
2022-10-31 00:08:52 -0500

    Fresh build.

--- build/block-visibility-settings.asset.php
+++ build/block-visibility-settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-block-library', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'b07e4e42b43ed1153c6d');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-block-library', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '6c6798fcaa616412256f');
