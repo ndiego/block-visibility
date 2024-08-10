c7d859240c4c1ba68cb99ade8175d6bd56b96bce
Nick Diego <nickmdiego@gmail.com>
2024-08-10 14:26:35 -0500

    Fix notice in Widget Editor.

--- includes/admin/editor.php
+++ includes/admin/editor.php
@@ -30,14 +30,9 @@ function enqueue_editor_scripts() {
 
 	// Remove wp-edit-post if on the Widgets screen, otherwise WordPress will throw an error.
 	if ( 'widgets' === get_current_screen()->id ) {
-		$dependencies = array_filter(
-			$dependencies,
-			function ( $dep ) {
-				return ( 'wp-edit-post' !== $dep );
-			}
-		);
+		$dependencies = array_diff( $dependencies, [ 'wp-editor', 'wp-edit-post', 'wp-edit-site' ] );
 	}
-
+	
 	wp_enqueue_script(
 		'block-visibility-editor-scripts',
 		BLOCK_VISIBILITY_PLUGIN_URL . 'build/block-visibility-editor.js',
